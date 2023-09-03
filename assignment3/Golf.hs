05872052f8667d0e710d57e8483ece91763af8b4
Jaron <jaronritter01@gmail.com>
2023-09-03 18:24:51 -0500

    finish assignment 3

--- assignment3/Golf.hs
+++ assignment3/Golf.hs
@@ -35,7 +35,51 @@ localMaxima :: [Integer] -> [Integer]
 localMaxima (first : second : third : remaining) = [second | second > first && second > third] ++ localMaxima (second : third : remaining)
 localMaxima _ = []
 
+{-
+Given a list of Intgers and value, count the number of occurrences of that value in the list.
+-}
+countOccs :: [Integer] -> Integer -> Integer
+countOccs [] _ = 0
+countOccs (num : nums) value =
+  if num == value
+    then 1 + countOccs nums value
+    else 0 + countOccs nums value
+
+{-
+Given a list of Integers, get the numbers of occurences of all numbers 0 to 9 and place those counts in the respective indexs.
+    e.g.: [1, 1, 1, 1, 5] -> [0, 4, 0, 0, 0, 1, 0, 0, 0, 0]
+-}
+getCountList :: [Integer] -> [Integer]
+getCountList nums = zipWith countOccs ([nums | value <- [0 .. 9]]) [0 ..]
+
+{-
+Generate a line in the histogram given the count list and the vertical line to generate.
+    e.g.: [0, 4, 0, 0, 0, 1, 0, 0, 0, 0] 0 -> " *   *    "
+    e.g.: [0, 4, 0, 0, 0, 1, 0, 0, 0, 0] 1 -> " *        "
+    e.g.: [0, 4, 0, 0, 0, 1, 0, 0, 0, 0] 7 -> "          "
+-}
+genLine :: [Integer] -> Integer -> String
+genLine [] _ = "\n"
+genLine (first : counts) num =
+  if first > num
+    then "*" ++ genLine counts num
+    else " " ++ genLine counts num
+
+{-
+Create a histogram by
+    1. Generating the count list
+    2. Representing the historgram as line starting from the bottom at index 0
+    3. Generate the correct string for each line 0 - 9
+    4. Concatenate those lines with newline
+    5. Concatenate that string with "==========\n" ++ "0123456789\n"
+-}
+histogram :: [Integer] -> String
+histogram nums = do
+  let countList = getCountList nums
+  concat [genLine countList index | index <- reverse [0 .. 9]] ++ "==========\n" ++ "0123456789\n"
+
 -- To Build ghc -main-is Golf -o main Golf.hs
 
 main :: IO ()
-main = print (localMaxima [2, 3, 4, 1, 5])
+main =
+  print (histogram [1, 1, 1, 1, 5])
