00a4e3741f1004fe8fc0727fd5b79a9abb798295
Jaron <jaronritter01@gmail.com>
2023-09-09 13:44:50 -0500

    mostly finish up assignment 4

--- assignment4/Main.hs
+++ assignment4/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import Data.List
+
+-- Exercise One
 fun1 :: [Integer] -> Integer
 fun1 [] = 1
 fun1 (x : xs)
@@ -15,7 +18,42 @@ fun2 n
   | even n = n + fun2 (n `div` 2)
   | otherwise = fun2 (3 * n + 1)
 
+fun2' :: Integer -> Integer
+fun2' =
+  sum
+    . filter even
+    . takeWhile (/= 1)
+    . iterate
+      ( \x ->
+          if even x
+            then x `div` 2
+            else x * 3 + 1
+      )
+
+-- Exercise 2 (Skipping for now)
+-- data Tree a
+--   = Leaf
+--   | Node Integer (Tree a) a (Tree a)
+--   deriving (Show, Eq)
+
+-- Need this to insert a node correctly
+-- insertNode :: Tree typ -> Tree typ
+-- insertNode tree = Leaf
+
+-- foldTree :: [a] -> Tree a
+-- foldTree a = Leaf
+
+-- Exercise 3-1
+xor :: [Bool] -> Bool
+xor = odd . foldr ((+) . fromEnum) 0
+
+-- Exercise 3-2
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x xs -> f x : xs) []
+
+-- Exercise 4
+seiveSundaram :: Integer -> [Integer]
+seiveSundaram n = filter odd [1 .. (2 * n + 2)] \\ sort (concat [[i + j + (2 * i * j) | i <- [1 .. j]] | j <- [1 .. n]])
+
 main :: IO ()
-main = do
-  print $ fun1 []
-  print $ fun1' []
+main = print $ seiveSundaram 5
