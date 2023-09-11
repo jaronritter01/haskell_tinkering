9fcde2fc8b3962a9bbb874f765d268a0ee708485
Jaron <jaronritter01@gmail.com>
2023-09-10 22:40:34 -0500

    assignment6 - complete exercise 1

---
+++ assignment6/Fibonacci.hs
@@ -0,0 +1,13 @@
+module Fibonacci where
+
+-- Exercise 1
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+fibs1 :: [Integer]
+fibs1 = map fib $ iterate (+ 1) 0
+
+main :: IO ()
+main = print fibs1
