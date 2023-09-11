1e7af4a2a1bc07049dbe1af558ca5c56c3dc24c6
Jaron <jaronritter01@gmail.com>
2023-09-11 18:19:14 -0500

    compelete exercise 2

--- assignment6/Fibonacci.hs
+++ assignment6/Fibonacci.hs
@@ -9,5 +9,13 @@ fib n = fib (n - 1) + fib (n - 2)
 fibs1 :: [Integer]
 fibs1 = map fib $ iterate (+ 1) 0
 
+-- Exercise 2
+fib' :: Integer -> [Integer] -> [Integer]
+fib' 0 list = list
+fib' n (ele1 : ele2 : _) = ele1 : fib' (n - 1) [ele2, ele1 + ele2]
+
+fibs2 :: [Integer]
+fibs2 = fib' 5 [0, 1]
+
 main :: IO ()
-main = print fibs1
+main = print fibs2
