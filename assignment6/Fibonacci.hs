8103a2535742eeb06a25bd07aff62850c8f9dc16
Jaron Ritter <jaronritter01@gmail.com>
2023-09-11 20:15:43 -0500

    work on assignment 6

--- assignment6/Fibonacci.hs
+++ assignment6/Fibonacci.hs
@@ -11,11 +11,36 @@ fibs1 = map fib $ iterate (+ 1) 0
 
 -- Exercise 2
 fib' :: Integer -> [Integer] -> [Integer]
-fib' 0 list = list
+fib' 0 list = init list
 fib' n (ele1 : ele2 : _) = ele1 : fib' (n - 1) [ele2, ele1 + ele2]
 
 fibs2 :: [Integer]
-fibs2 = fib' 5 [0, 1]
+fibs2 = fib' 1 [0, 1]
+
+-- Exercise 3
+data Stream a = Stream a (Stream a)
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+streamToList :: Stream a -> [a]
+streamToList (Stream first rest) = first : streamToList rest
+
+-- Exercise 4
+streamRepeat :: a -> Stream a
+streamRepeat x = Stream x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap func (Stream first rest) = Stream (func first) (streamMap func rest)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed func x = Stream x (streamFromSeed func (func x))
+
+-- Exercise 5
+nats :: Stream Integer
+nats = streamFromSeed (+1) 0
+
+ruler :: Stream Integer
 
 main :: IO ()
-main = print fibs2
+main = print nats
