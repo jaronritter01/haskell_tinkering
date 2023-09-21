988f64a975cb8815925f987fdca05d876b6cada9
Jaron <jaronritter01@gmail.com>
2023-09-21 07:20:26 -0500

    start assignment 7

--- assignment7/JoinList.hs
+++ assignment7/JoinList.hs
@@ -4,8 +4,10 @@ data JoinList m a
   | Append m (JoinList m a) (JoinList m a)
   deriving (Eq, Show)
 
--- (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 tag :: Monoid m => JoinList m a -> m
 tag Empty = mempty
 tag (Single m _) = m
 tag (Append m _ _) = m
+
+(+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
+(+++) list1 list2 = Append (tag list1 <> tag list2) list1 list2
