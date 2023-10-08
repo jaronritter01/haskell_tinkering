566fa01c6dda7391f4b097c9f7d036d15950d318
Jaron <jaronritter01@gmail.com>
2023-10-08 13:12:38 -0500

    work a little more

--- assignment7/JoinList.hs
+++ assignment7/JoinList.hs
@@ -3,7 +3,7 @@
 {-# HLINT ignore "Use isNothing" #-}
 module JoinList where
 
-import           Sized
+import Sized
 
 data JoinList m a
   = Empty
@@ -12,8 +12,8 @@ data JoinList m a
   deriving (Eq, Show)
 
 tag :: Monoid m => JoinList m a -> m
-tag Empty          = mempty
-tag (Single m _)   = m
+tag Empty = mempty
+tag (Single m _) = m
 tag (Append m _ _) = m
 
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
@@ -26,7 +26,8 @@ indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
 indexJ _ Empty = Nothing
 indexJ i _ | i < 0 = Nothing
 indexJ _ (Single _ innerVal) = Just innerVal
-indexJ i joinList | i >= getSizeTag joinList = Nothing
+indexJ i joinList
+  | i >= getSizeTag joinList = Nothing
 indexJ index (Append currentTag lhs rhs)
   | index < (getSize (size currentTag) `div` 2) = indexJ index lhs
   | otherwise =
@@ -39,8 +40,7 @@ testIndexJ :: Bool
 testIndexJ = do
   let testInput = Append (Size 4) (Append (Size 2) (Single (Size 1) "a") (Single (Size 1) "b")) (Append (Size 2) (Single (Size 1) "c") (Single (Size 1) "d"))
   and
-    [
-      indexJ (-1) testInput == Nothing,
+    [ indexJ (-1) testInput == Nothing,
       indexJ 0 testInput == Just "a",
       indexJ 1 testInput == Just "b",
       indexJ 2 testInput == Just "c",
@@ -50,8 +50,17 @@ testIndexJ = do
     ]
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
-dropJ index joinList | index <= 0 = joinList
-dropJ _ _ = Empty
+dropJ index joinList -- Bounds checking
+  | index <= 0 = joinList
+  | index >= getSizeTag joinList = Empty
+dropJ _ (Single _ _) = Empty -- Passed bounds check and drop anything on a single
+dropJ index (Append cachedVal lhs rhs)
+  | index == getSize (size cachedVal) = Empty
+  | index <= (getSize (size cachedVal) `div` 2) =
+      let newLhs = dropJ index lhs
+       in Append (tag newLhs <> tag rhs) newLhs rhs
+
+{- TODO: finish the right hand side of this equation -}
 
 main :: IO ()
 main = print testIndexJ
